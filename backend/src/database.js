15f54bbc2285424ee73a5b3e86a04f46387b5c73
mariano <marianodacev@gmail.com>
2020-07-08 13:27:28 -0300

    commit

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI: 'mongodb://localhost:27017';
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI: process.env.MONGODB_URI;
+console.log(`DB HOST => ${URI}`);
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
